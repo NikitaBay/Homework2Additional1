ddbd91c289342bc7384cc559a0c0dcc1bb8e1a81
NikitaBay <n.bayushev2017@yandex.ru>
2023-11-02 18:53:56 +0400

    Initial commit

---
+++ src/main/java/Primitives.java
@@ -0,0 +1,12 @@
+public class Primitives {
+    public static void main(String[] args) {
+        byte a = 20;
+        short b = 200;
+        int c = 20000;
+        long d = 2000000000L;
+        double e = 200.22;
+        float f = 20.2F;
+        boolean g = true;
+        char h = 'H';
+    }
+}
---
+++ src/main/java/Wrappers.java
@@ -0,0 +1,12 @@
+public class Wrappers {
+    public static void main(String[] args) {
+        Byte a = new Byte(20);
+        Short b = new Short(200);
+        Integer c = new Integer(20000);
+        Long d = new Long(2000000000L);
+        Double e = new Double(200.22);
+        Float f = new Float(20.2F);
+        Boolean g = new Boolean(true);
+        Character h = new Character('H');
+    }
+}
